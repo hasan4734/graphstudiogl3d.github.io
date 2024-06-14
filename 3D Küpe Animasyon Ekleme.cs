325017dde06607367b26a7594224477a7d2e0fb7
hasan4734 <164076910+hasan4734@users.noreply.github.com>
2024-06-14 18:32:05 +0300

    Create 3D Küpe Animasyon Ekleme.cs

---
+++ 3D Küpe Animasyon Ekleme.cs
@@ -0,0 +1,33 @@
+public MainForm()
+{
+ //Döngüyü Başlatma
+  Run.Start();
+//Render Oluşturma
+  Renderer rederer = new Renderer(800,600);
+
+//Camera Oluşturma
+ camera = new Camera();
+ camera.Position = new Vector3(0, 0, 5);
+ camera.Target = new Vector3(0, 0, 0);
+ camera.Up = new Vector3(1, 0, 0);
+
+//Küp Ve Material Oluşturma
+ Material Cubemat = new Material(Color.LightSkyBlue,10,DifusseTexture,BumpTexture);
+ Cube cube = new Cube(new Vector3(0, 0, 0), 1);
+ cube.Material = Cubemat;
+ cube.Material.Shininess=0.1f;
+ cube.Name ="Cube";
+}
+//Render Döngüsü
+ void RunTick(object sender, EventArgs e)
+  {
+    	gl.Clear(Color.Black);
+      //Küpü Çizme
+      cube.Draw(renderer,camera);
+      //Küpe Animasyon Ekleme
+      cube.RotateX(0.07);
+      cube.RotateY(0.07);
+      cube.RotateZ(0.07);
+
+			pictureBox1.Image =gl.Render(Cubemat);
+  }
